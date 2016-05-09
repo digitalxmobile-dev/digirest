efda1509add597289e8c9216e68016bc341f1062
auridevil <onailerua@hotmail.com>
2016-05-09 15:03:47 +0200

    git fixes

---
+++ digitest/digitest.js
@@ -0,0 +1,11 @@
+/**
+ * Created by mox on 09/05/16.
+ */
+
+var assert = require('assert');
+
+/** init express env */
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+
